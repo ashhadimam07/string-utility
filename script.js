5cc38de027bdf8f6e6a3b1835cb954be4bd526e4
ashhadimam07 <ashhadimam07@gmail.com>
2021-02-11 13:37:42 +0530

    Create string utility

---
+++ script.js
@@ -0,0 +1,27 @@
+const lowercase = document.querySelector('#lowercase');
+const uppercase = document.querySelector('#uppercase');
+const reverse = document.querySelector('#reverse');
+const charcount = document.querySelector('#charcount');
+const wordcount = document.querySelector('#wordcount');
+const input = document.querySelector('#input-string');
+const output = document.querySelector('#output-string');
+
+lowercase.addEventListener('click', () => {
+    output.value = input.value.toLowerCase();
+});
+
+uppercase.addEventListener('click', () => {
+    output.value = input.value.toUpperCase();
+});
+
+charcount.addEventListener('click', () => {
+    output.value = parseInt(input.value.length, 10);
+});
+
+reverse.addEventListener('click', () => {
+    output.value = input.value.split('').reverse().join('');
+});
+
+wordcount.addEventListener('click', () => {
+    output.value = parseInt(input.value.split(' ').length, 10);
+});
